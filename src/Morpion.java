763c22db8264796c0c12e4bb284ce55f8a37f414
Etienne Marlier <etienne.marlier@etudiant.univ-lille1.fr>
2015-09-03 08:36:42 +0200

    deux modif

--- src/Morpion.java
+++ src/Morpion.java
@@ -3,13 +3,11 @@ public class Morpion extends Plateau {
 
 	static final int hauteur = 3;
 	static final int largeur = 3;
-	char[][] petitMorpion = new char[hauteur][largeur];
+	char[] petitMorpion = new char[hauteur*largeur];
 	
 	Morpion () {
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < largeur; j++) {
-				this.petitMorpion[i][j] = ' ';
-			}
+		for (int i = 0; i < hauteur*largeur; i++) {
+				this.petitMorpion[i] = ' ';
 		}
 	}
 	
@@ -32,14 +30,45 @@ public class Morpion extends Plateau {
 		return rep;
 	}*/
 	
-	public void addChar(int abs, int ord, Joueur j) {
-		if (this.petitMorpion[ord][abs] == ' ') {
-			this.petitMorpion[ord][abs] = j.getMarque();
+	public void addChar(int num, Joueur j) {
+		if (this.petitMorpion[num] == ' ') {
+			this.petitMorpion[num] = j.getMarque();
 		}
 	}
 	
-	public char getChar(int abs, int ord) {
-		return petitMorpion[ord][abs];
+	public boolean isFini() {
+		char[][] test = this.getPetitMorpion();
+		if (test[0] != ' ' && test[0] == test[0][1] && test [0][2] == test[0][1]) { // Ligne verticale de gauche
+			return true;
+		} else if (test[0][0] != ' ' && test[0][0] == test[1][0] && test[0][0] == test[2][0]) { // Ligne horizontale du haut
+			return true;
+		} else if (test[0][0] != ' ' && test[0][0] == test[1][1] && test[0][0] == test[2][2]) { // Ligne diagonale de haut gauche vers bas droite
+			return true;
+		} else if (test[0][1] != ' ' && test[1][0] == test[1][1] && test[1][0] == test[1][2]) { // Ligne verticale du milieu
+			return true;
+		} else if (test[2][0] != ' ' && test[2][0] == test[2][1] && test[2][0] == test[2][2]) { // Ligne verticale de droite
+			return true;
+		} 
+	}
+	
+	public char[] getPetitMorpion() {
+		return petitMorpion;
+	}
+
+	public void setPetitMorpion(char[] petitMorpion) {
+		this.petitMorpion = petitMorpion;
+	}
+
+	public static int getHauteur() {
+		return hauteur;
+	}
+
+	public static int getLargeur() {
+		return largeur;
+	}
+
+	public char getChar(int num) {
+		return petitMorpion[num];
 	}
 	
 	
