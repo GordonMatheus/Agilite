21c1294eae3554e2ac4f4ce02db985311e4a37db
Deltour <lucas.deltour95@gmail.com>
2015-09-03 14:30:17 +0200

    update main

--- src/main.java
+++ src/main.java
@@ -1,29 +1,36 @@
-
 public class main {
 	public static void main(String[] args) {
-		BigMorpion m = new BigMorpion();
-		Joueur j1 = new Joueur("Paul", 'X');
-		Joueur j2 = new Joueur("Jean", 'O');
-		
-		System.out.println("Bonjour, bienvenue dans Mega Morpion \n" +
-				"Le but du jeu est de gagner 3 petits morpions alignés dans le grand plateau pour gagner le grand morpion \n" +
-				"Le premier joueur choisi où il peut jouer, et le deuxième joueur devra jouer selon le placement du pion du premier joueur \n" +
-				"En effet, si le premier joueur place son pion dans le carré haut-gauche d'un petit morpion, le deuxième joueur devra donc jouer\n" +
-				"sur la case haut-gauche du grand morpion, et décidera donc du petit morpion où le premier joueur jouera son second tour, \n hggrrrrrrrr" +
-				"et ainsi de suite jusqu'à ce que l'on réussisse à aligner 3 petits morpions gagnants.");
-		System.out.println("Si un morpion a été rempli mais qu'il n'y a pas de gagnant (match nul), il sera rempli de 'N");
-		System.out.println("Si le joueur ne peut pas jouer sur le morpion assigné par le joueur précédent, car celui ci est plein \n" +
-				"alors, il devra jouer sur le morpion où le joueur précédent a joué");
-		System.out.println("L'ordinateur vous dira dans quel morpion jouer. Vous devrez entrer un numéro entre 1 et 9, les cases se situeront selon ce dessin: \n" + 
-				" 1 | 2 | 3 \n" +
-				" 4 | 5 | 6 \n" + 
-				" 7 | 8 | 9 \n");
-		
-		m.getTotal()[0].addChar(0,j1);
-		m.getTotal()[0].addChar(1,j1);
-		System.out.println(m.getTotal()[0].isFini());
-		m.getTotal()[0].addChar(2,j1);
-		System.out.println(m.getTotal()[0].isFini());
-		System.out.println(m.getTotal()[0]);
+
+		System.out
+				.println("Bonjour, bienvenue dans Mega Morpion \n"
+						+ "Le but du jeu est de gagner 3 petits morpions alignés dans le grand plateau pour gagner le grand morpion \n"
+						+ "Le premier joueur choisi où il peut jouer, et le deuxième joueur devra jouer selon le placement du pion du premier joueur \n"
+						+ "En effet, si le premier joueur place son pion dans le carré haut-gauche d'un petit morpion, le deuxième joueur devra donc jouer\n"
+						+ " sur la case haut-gauche du grand morpion, et décidera donc du petit morpion où le premier joueur jouera son second tour,\n"
+						+ "et ainsi de suite jusqu'à ce que l'on réussisse à aligner 3 petits morpions gagnants.\n"
+						+ "Si un morpion a été rempli mais qu'il n'y a pas de gagnant (match nul), il sera rempli de N\n"
+						+ "Si le joueur ne peut pas jouer sur le morpion assigné par le joueur précédent, car celui ci est plein \n"
+						+ "alors, il devra choisir le morpion sur lequel il veut jouer\n");
+
+		Partie p = new Partie();
+		Joueur j1 = new Joueur('X');
+		j1.chooseName();
+		Joueur j2 = new Joueur('O');
+		j2.chooseName();
+
+		System.out.println("Début de la partie");
+
+		p.start(j1, j2);
+
+		System.out.println("Fin de la partie \n");
+
+		char winner = p.checkWinner();
+		if (winner == 'N')
+			System.out.println("égalité");
+		else if (winner == j1.getMarque())
+			System.out.println("Le joueur " + j1.getNom() + "a gagné");
+		else
+			System.out.println("Le joueur " + j2.getNom() + "a gagné");
+
 	}
 }
