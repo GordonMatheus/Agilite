fb08606c9be4b9da155d3a568cd1a333a6f7fcf6
Deltour <lucas.deltour95@gmail.com>
2015-09-04 11:37:16 +0200

    updated tests

--- test/TestBigMorpion.java
+++ test/TestBigMorpion.java
@@ -1,4 +1,4 @@
-package Test;
+package test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -10,13 +10,14 @@ import morpion.Joueur;
 import morpion.Morpion;
 
 public class TestBigMorpion {
+
 	@Test
-	public final void TestisFini() {
+	public final void isFini() {
 
 		BigMorpion m = new BigMorpion();
 		Morpion[] l = m.getTotal();
-		Joueur j1 = new Joueur("j1", 'I');
-		Joueur j2 = new Joueur("j2", 'x');
+		Joueur j1 = new Joueur('I');
+		Joueur j2 = new Joueur('x');
 
 		assertFalse(m.isFini());
 
@@ -35,12 +36,13 @@ public class TestBigMorpion {
 		assertTrue(m.isFini());
 
 	}
+
 	@Test
-	public final void TestisRempli() {
+	public final void isRempli() {
 		BigMorpion m = new BigMorpion();
 		Morpion[] l = m.getTotal();
-		Joueur j1 = new Joueur("j1", 'I');
-		Joueur j2 = new Joueur("j2", 'x');
+		Joueur j1 = new Joueur('I');
+		Joueur j2 = new Joueur('x');
 
 		for (int i = 0; i < 9; i++) {
 			l[0].addChar(i, j1);
@@ -59,26 +61,28 @@ public class TestBigMorpion {
 		}
 		assertTrue(m.isRempli());
 	}
+
 	@Test
-	public final void testAffichageLigne() {
+	public final void affichageLigne() {
 		BigMorpion m = new BigMorpion();
 		Morpion[] l = m.getTotal();
-		Joueur j1 = new Joueur("j1", 'I');
-		Joueur j2 = new Joueur("j2", 'x');
-		
+		Joueur j1 = new Joueur('I');
+		Joueur j2 = new Joueur('x');
+
 		l[0].addChar(0, j1);
 		l[0].addChar(1, j1);
 		l[0].addChar(2, j2);
-		
+
 		l[1].addChar(0, j1);
 		l[1].addChar(1, j2);
 		l[1].addChar(2, j2);
-		
+
 		l[2].addChar(1, j1);
-		
+
+		System.out.println(m.afficherLigne(0, 0));
 		String resultatTheorique = "";
 		assertFalse(resultatTheorique.equals(m.afficherLigne(0, 0)));
 		resultatTheorique = "I|I|x || I|x|x ||  |I| ";
 		assertTrue(resultatTheorique.equals(m.afficherLigne(0, 0)));
-		}
+	}
 }
--- test/TestMorpion.java
+++ test/TestMorpion.java
@@ -1,6 +1,4 @@
-
-
-package Test;
+package test;
 
 import static org.junit.Assert.*;
 import morpion.Joueur;
@@ -9,50 +7,47 @@ import morpion.Morpion;
 import org.junit.Test;
 
 public class TestMorpion {
-	
+
 	@Test
-	public final void TestisFini(){
-		
+	public final void isFini() {
+
 		Morpion m = new Morpion();
-		Joueur j1 = new Joueur("j1",'I');
-		Joueur j2 = new Joueur("j2",'x');
-		
+		Joueur j1 = new Joueur('I');
+		Joueur j2 = new Joueur('x');
+
 		assertFalse(m.isFini());
-		
+
 		m.addChar(0, j1);
-		m.addChar(1,j1);
-		m.addChar(3,j1);
+		m.addChar(1, j1);
+		m.addChar(3, j1);
 		assertFalse(m.isFini());
-		
+
 		m.addChar(2, j2);
 		assertFalse(m.isFini());
-		m.addChar(5,j2);
-		m.addChar(8,j2);
+		m.addChar(5, j2);
+		m.addChar(8, j2);
 		assertTrue(m.isFini());
-		
-		
 	}
+
 	@Test
-	public final void TestisRempli(){
-		
+	public final void isRempli() {
+
 		Morpion m = new Morpion();
-		Joueur j1 = new Joueur("j1",'I');
+		Joueur j1 = new Joueur('I');
 
 		assertFalse(m.isRempli());
 		m.addChar(0, j1);
-		m.addChar(1,j1);
-		m.addChar(2,j1);
+		m.addChar(1, j1);
+		m.addChar(2, j1);
 		m.addChar(3, j1);
-		m.addChar(5,j1);
-		m.addChar(6,j1);
+		m.addChar(5, j1);
+		m.addChar(6, j1);
 		m.addChar(7, j1);
-		m.addChar(8,j1);
+		m.addChar(8, j1);
 		assertFalse(m.isRempli());
-		m.addChar(4,j1);
+		m.addChar(4, j1);
 		assertTrue(m.isRempli());
-		
+
 	}
-	
-	
 
 }
